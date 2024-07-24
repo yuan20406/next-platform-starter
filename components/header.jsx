307d60e407a925cc7e63d84689c92945ec396cf0
yuan20406 <yuan20406@126.com>
2024-07-24 15:21:28 +0800

    Update header.jsx

--- components/header.jsx
+++ components/header.jsx
@@ -4,7 +4,7 @@ import netlifyLogo from 'public/netlify-logo.svg';
 import githubLogo from 'public/images/github-mark-white.svg';
 
 const navItems = [
-    { linkText: 'Home', href: '/' },
+    { linkText: 'Homeeeeee', href: '/' },
     { linkText: 'Revalidation', href: '/revalidation' },
     { linkText: 'Image CDN', href: '/image-cdn' },
     { linkText: 'Edge Function', href: '/edge' },
